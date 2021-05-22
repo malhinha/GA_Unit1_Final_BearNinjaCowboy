75af873869d5f217dd24e6e0aba4544cca08ab06
John Malhinha <john.malhinha@gmail.com>
2021-05-22 10:28:49 -0700

    Added round winner declaration and replay game option

--- Bear_Ninja_Cowboy/js/script.js
+++ Bear_Ninja_Cowboy/js/script.js
@@ -1,4 +1,6 @@
-// cache dom nodes
+/* ======================
+CACHE DOM NODES
+=========================*/
 
 const ninjaBtn = document.getElementById('ninja');
 const bearBtn = document.getElementById('bear');
@@ -6,8 +8,12 @@ const cowboyBtn = document.getElementById('cowboy');
 const resultDiv = document.getElementById('result');
 const compScoreDisplay = document.getElementById('computer_score');
 const playerScoreDisplay = document.getElementById('player_score');
+const gamePlayArea = document.getElementById('gamePlayArea');
 
-// array of options, play patterns
+
+/* ======================
+DATA ARRAYS
+=========================*/
 
 const choiceOptions = [
   'ninja', 'bear', 'cowboy'
@@ -17,7 +23,10 @@ const easyGamePlay = [
   0, 1, 0, 2, 0, 0
 ];
 
-// global variables for player choices, scores, gameplay indexes
+
+/* ======================
+GLOBAL VARS
+=========================*/
 
 let computer = '';
 let player = '';
@@ -28,17 +37,42 @@ let easyPlayIndex = 0;
 let medPlayIndex = 0;
 
 
-// see who wins
+/* ======================
+GAME FUNCTIONS
+=========================*/
+
+// reset global variables for a new game
+
+const gameReset = () => {
+  computer = '';
+  player = '';
+  computerScore = 0;
+  playerScore = 0;
+  gamePlay = '';
+  easyPlayIndex = 0;
+  medPlayIndex = 0;
+}
+
+
+// create the gameboard for a new game
+
+const newGame = () => {
+  console.log('ready to start');
+}
+
+
+// see who wins match
 
 const matchResults = (playerChoice) => {
 
   let compChoice = generateCompChoice();
 
+  // for debugging
   player = choiceOptions[playerChoice];
   computer = choiceOptions[compChoice];
-
   console.log(`player: ${player}`);
   console.log(`computer: ${computer}`);
+  //
 
   if ((playerChoice == 0 && compChoice == 1) ||
   (playerChoice == 1 && compChoice == 2) ||
@@ -52,10 +86,12 @@ const matchResults = (playerChoice) => {
     console.log(`It's a draw :(`);
     resultDiv.innerHTML = 'It\'s a draw :(';
   }
+
+  roundCheck();
 }
 
 
-// have computer make their choice
+// have computer make their choice & return back to matchResults
 
 const generateCompChoice = () => {
 
@@ -63,8 +99,8 @@ const generateCompChoice = () => {
     //sets computer choice to random selection from options array
     let choice = Math.floor(Math.random() * choiceOptions.length);
     return choice;
-  }
-  else if (gamePlay == 'easy') {
+
+  } else if (gamePlay == 'easy') {
     //sets computer choice to current easy play index selection
     let choice = easyGamePlay[easyPlayIndex];
 
@@ -76,11 +112,15 @@ const generateCompChoice = () => {
     }
 
     return choice;
+
+  } else {
+    console.log('ERROR: gamePlay not set');
   }
 
 }
 
-// reflect who wins/update points
+
+// reflect who wins match/update points
 
 const computerWins = () => {
   computerScore++;
@@ -103,6 +143,56 @@ const updateScore = () => {
   playerScoreDisplay.innerHTML = playerScore;
 }
 
+
+// checks to see if someone won the round
+
+const roundCheck = () => {
+  if (computerScore === 5) {
+    // wipe away game control buttons & global vars
+    gamePlayArea.innerHTML = 'The computer wins!';
+    gameReset();
+
+    // create 'play again' button
+    createStartButton('PLAY AGAIN?');
+
+  } else if (playerScore === 5) {
+    // wipe away game control buttons
+    gamePlayArea.innerHTML = 'You beat the computer!';
+    gameReset();
+
+    // create 'play again' button
+    createStartButton('PLAY AGAIN?');
+
+  }
+}
+
+
+// creates new game start button
+
+const createStartButton = (label) => {
+  const newStartButton = document.createElement('button');
+  newStartButton.id = "start_game";
+  newStartButton.innerHTML = label;
+  gamePlayArea.appendChild(newStartButton);
+
+  const startButton = document.getElementById('start_game');
+
+  startButton.addEventListener('click', (e) => {
+    newGame();
+  });
+}
+
+
+/* ======================
+EVENT LISTENERS
+=========================*/
+
+// start new game
+
+// startButton.addEventListener('click', (e) => {
+//   newGame();
+// });
+
 // have player make their choices
 
 ninjaBtn.addEventListener('click', (e) => {
@@ -116,3 +206,21 @@ bearBtn.addEventListener('click', (e) => {
 cowboyBtn.addEventListener('click', (e) => {
   matchResults(2);
 });
+
+
+/* psuedo game flow code scratchsheet
+set global vars (empty)
+draw start button [click]
+draw difficulty level buttons [click]
+match play:
+- draw play options [click]
+- play match
+- wipe play options/declare winner/(?draw go again?[click])
+- repeat
+end of round/declare winner/draw replay button
+reset global vars
+draw difficulty level buttons...
+
+
+
+*/
